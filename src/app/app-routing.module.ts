7452caa317fc5bd4893186d0ec585ca014b238e6
CETECOM <cetecom_conce@duoc.cl>
2024-08-22 10:07:37 -0400

    rutas ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+import { NotFoundError } from 'rxjs';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },{
+    path:'home',
+    component: HomeComponent
+  },{
+    path:'contacto',
+    component: ContactoComponent
+  },{
+    path:'ayuda',
+    component: AyudaComponent
+  },{
+    path:'**',
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
